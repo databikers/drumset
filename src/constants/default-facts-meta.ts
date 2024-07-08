e6583a0b51da8ec3672cf2912320e4e96d3c188e
pashaman <plavel@gmail.com>
2024-07-08 17:42:03 +0700

    fix: decrease default retries limit to zero

--- src/constants/default-facts-meta.ts
+++ src/constants/default-facts-meta.ts
@@ -2,7 +2,7 @@ import { FactsMetaKeys } from './facts-meta-keys';
 
 export const defaultFactsMeta: Record<FactsMetaKeys, number> = {
   [FactsMetaKeys.RETRIES]: 0,
-  [FactsMetaKeys.RETRIES_LIMIT]: 1,
+  [FactsMetaKeys.RETRIES_LIMIT]: 0,
   [FactsMetaKeys.EXPIRE_AFTER]: 0,
   [FactsMetaKeys.EXECUTE_AFTER]: 0
 }
