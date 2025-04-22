105a68e7d847699e0362ea90e8fa5a4c43d8e53c
pashaman <plavel@gmail.com>
2025-04-22 20:10:11 +0700

    feat(feature/parallelism): updated facts contract

--- src/parameters/facts.ts
+++ src/parameters/facts.ts
@@ -3,11 +3,16 @@ import { FactsStatus } from '@const';
 
 export type Facts<DataType, NodeName extends string> = {
   id: string;
-  currentNode: NodeName;
+  processedNodes: Set<NodeName>;
+  enqueuedNodes: Set<NodeName>;
+  failedNodes: Set<NodeName>;
+  nodeErrors: Map<NodeName, Error>;
   data: DataType;
-  meta: FactsMeta;
+  meta: FactsMeta<NodeName>;
   status: FactsStatus;
-  stats: Partial<Record<FactsStatus, number>> & { retries?: number };
-  inUse: boolean;
+  stats: Partial<Record<FactsStatus, number>> & { retries?: Map<NodeName, number> };
+  activeCompensator: Set<NodeName>;
+  rollbacks: Set<NodeName>;
+  inUse: Set<NodeName>;
   used: boolean;
 };
