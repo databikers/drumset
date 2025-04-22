9733eda820a12dd92a01b3e9065ba2ff0e9a2841
pashaman <plavel@gmail.com>
2025-04-22 20:31:40 +0700

    feat(feature/parallelism): updated processing under the hood

--- src/framework/framework.ts
+++ src/framework/framework.ts
@@ -1,7 +1,7 @@
+import { defaultFactsMeta, FactsStatus } from '@const';
 import { FrameworkOptions } from '@options';
-import { Facts, FactsMeta } from '@parameters';
+import { Facts, NodeMeta } from '@parameters';
 import { FrameworkInterface } from './framework-interface';
-import { FactsStatus } from '@const';
 
 export class Framework<DataType, NodeName extends string> implements FrameworkInterface<DataType, NodeName> {
   protected options: FrameworkOptions<DataType, NodeName>;
@@ -9,44 +9,56 @@ export class Framework<DataType, NodeName extends string> implements FrameworkIn
     this.options = frameworkOptions;
   }
 
-  public next(node: NodeName, facts: Facts<DataType, NodeName>) {
-    if (!this.options.nodes.has(node)) {
-      return this.exit(facts, new Error(`Node ${node} doesn't exist`));
+  public next(node: NodeName | NodeName[], facts: Facts<DataType, NodeName>) {
+    let nodes = Array.isArray(node) ? node.filter((item: any) => typeof item === 'string' && item) : [node];
+    nodes = nodes.filter((item: any) => typeof item === 'string' && item);
+    if (!nodes.length) {
+      return this.exit(facts, new Error('next function requires a non-empty array/string'));
     }
     if (facts) {
-      const { id, currentNode } = facts;
-      if (!facts.inUse && !facts.used) {
-        facts.inUse = true;
-        if (facts.currentNode !== node) {
-          const defaultMeta: FactsMeta = this.options.meta.get(node);
-          const {
-            compensatorNode,
-            expireAfter,
-            executeAfter,
-            timeoutBetweenRetries,
-            retries,
-            retriesLimit,
-            lastRetryTime,
-          } = facts.meta;
-          facts.currentNode = node;
-          facts.meta = {
-            expireAfter: expireAfter || defaultMeta.expireAfter,
-            executeAfter: executeAfter || defaultMeta.executeAfter,
-            retries: retries || defaultMeta.retries,
-            retriesLimit: retriesLimit || defaultMeta.retriesLimit,
-            timeoutBetweenRetries: timeoutBetweenRetries || defaultMeta.timeoutBetweenRetries,
-            compensatorNode: compensatorNode || defaultMeta.compensatorNode,
-            lastRetryTime,
-          };
-          facts.meta = { ...facts.meta, ...this.options.meta.get(node) };
-        } else {
-          facts.meta.lastRetryTime = facts.meta.lastRetryTime || new Date().getTime();
+      for (const n of nodes) {
+        if (!this.options.nodes.has(n)) {
+          return this.exit(facts, new Error(`Node ${n} doesn't exist`));
         }
-        this.options.nodes.get(node).process(facts);
-      } else if (facts.used && this.options.verbose) {
-        this.options.logger.log(`Saga has been finished for facts: ${id}, currentNode: ${currentNode}, node: ${node})`);
-      } else if (facts.inUse && this.options.verbose) {
-        this.options.logger.log(`Facts ${id} has being taken already, currentNode: ${currentNode}, node: ${node})`);
+        if (facts.processedNodes.has(n) || (facts.used && !facts.rollbacks.has(n) && facts.activeCompensator.has(n))) {
+          continue;
+        }
+        const nodeMeta: NodeMeta = this.options.meta.get(n);
+        if (nodeMeta?.runAfterNodes?.length) {
+          const needWait: boolean = nodeMeta?.runAfterNodes.some((name: NodeName) => !facts.processedNodes.has(name));
+          if (needWait) {
+            const needExit: boolean = nodeMeta?.runAfterNodes.some((name: NodeName) => facts.failedNodes.has(name));
+            if (needExit) {
+              const [ errorNode ] = nodeMeta?.runAfterNodes.filter((name: NodeName) => facts.failedNodes.has(name)) || [];
+              return this.exit(facts, errorNode ? facts.nodeErrors.get(errorNode as NodeName) : undefined)
+            }
+            continue;
+          }
+        }
+        if (!facts.meta.has(n)) {
+          facts.meta.set(n, {
+            ...defaultFactsMeta,
+            ...nodeMeta,
+            node: n,
+          });
+        } else {
+          facts.meta.get(n).node = n;
+        }
+        const meta = facts.meta.get(n);
+        meta.retrying = false;
+        const { id } = facts;
+        if (
+          (!facts.inUse.size || nodes.includes(n)) &&
+          (!facts.used || facts.activeCompensator.has(n) || facts.rollbacks.has(n))
+        ) {
+          facts.inUse.add(n);
+          meta.lastRetryTime = new Date().getTime();
+          this.options.nodes.get(n).process(facts);
+        } else if (facts.used && this.options.verbose) {
+          this.options.logger.log(`Saga has been finished for facts: ${id}, node: ${n})`);
+        } else if (facts.inUse.size && this.options.verbose) {
+          this.options.logger.log(`Facts ${id} has being taken already, ${n}, node: ${n})`);
+        }
       }
     }
   }
@@ -55,17 +67,31 @@ export class Framework<DataType, NodeName extends string> implements FrameworkIn
     facts.used = true;
     facts.status = FactsStatus.PROCESSED;
     facts.stats[FactsStatus.PROCESSED] = new Date().getTime();
+    if (error) {
+      facts.rollbacks.forEach((node: NodeName) => {
+        this.next(node, facts);
+      });
+    }
     this.options.eventEmitter.emit(facts.id, error, facts.data);
   }
 
   public retry(node: NodeName, facts: Facts<DataType, NodeName>, error?: Error) {
-    const { retries, retriesLimit } = facts.meta;
-    if (retries < retriesLimit) {
-      facts.meta.retries = (facts.meta.retries || 0) + 1;
-      facts.stats.retries = facts.meta.retries;
-      this.next(node, facts);
-    } else {
-      this.exit(facts, error || new Error(`Exceeded the limit of retries at "${node}" node`));
+    facts.inUse.delete(node);
+    const meta = facts.meta.get(node);
+    const { retries, retriesLimit } = meta;
+    if (retries < retriesLimit && !facts.used) {
+      meta.retrying = true;
+      return this.next(node, facts);
     }
+    facts.failedNodes.add(node);
+    facts.nodeErrors.set(node, error);
+    if (meta.rollbackWhenErrorNode) {
+      facts.activeCompensator.add(meta.rollbackWhenErrorNode);
+      if (meta.rollbackWhenSuccessNode) {
+        facts.rollbacks.delete(meta.rollbackWhenSuccessNode);
+      }
+      return this.next(meta.rollbackWhenErrorNode as NodeName, facts);
+    }
+    this.exit(facts, error || new Error(`Exceeded the limit of retries at '${node}' node`));
   }
 }
