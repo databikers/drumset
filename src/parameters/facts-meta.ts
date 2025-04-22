4ad99c2b6494016295d4c6fe3bb49c87912313f8
pashaman <plavel@gmail.com>
2025-04-22 20:09:30 +0700

    feat(feature/parallelism): updated facts meta contracts

--- src/parameters/facts-meta.ts
+++ src/parameters/facts-meta.ts
@@ -1,9 +1,15 @@
-export type FactsMeta = {
-  expireAfter: number;
-  executeAfter: number;
+export type FactsMetaContract<NodeName> = {
+  node: NodeName;
+  expireAfter?: number;
+  executeAfter?: number;
   retries: number;
   retriesLimit: number;
-  timeoutBetweenRetries: number;
-  lastRetryTime: number;
-  compensatorNode?: string;
+  timeoutBetweenRetries?: number;
+  lastRetryTime?: number;
+  rollbackWhenErrorNode?: NodeName;
+  rollbackWhenSuccessNode?: NodeName;
+  retrying?: boolean;
+  runAfterNodes: NodeName[];
 };
+
+export type FactsMeta<NodeName extends string> = Map<NodeName, FactsMetaContract<NodeName>>;
