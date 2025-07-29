cfc26ac61639a1939691f9ab764b1fa3a9f4e2c0
pashaman <plavel@gmail.com>
2025-07-29 22:36:50 +0700

    fix: immediately call executor first time

--- src/helpers/pseudo-interval.ts
+++ src/helpers/pseudo-interval.ts
@@ -1,16 +1,31 @@
 import { PseudoIntervalParams } from '@parameters';
 
-export function pseudoInterval(pseudoIntervalParams: PseudoIntervalParams) {
+function loop(pseudoIntervalParams: PseudoIntervalParams) {
   const { executor, isRan, doExit, interval } = pseudoIntervalParams;
+
   setTimeout(async () => {
     if (isRan) {
       executor().finally(() => {
         if (!doExit) {
-          pseudoInterval(pseudoIntervalParams);
+          loop(pseudoIntervalParams);
         }
       });
     } else if (!doExit) {
-      pseudoInterval(pseudoIntervalParams);
+      loop(pseudoIntervalParams);
     }
   }, interval || 0);
+}
+
+export function pseudoInterval(pseudoIntervalParams: PseudoIntervalParams) {
+  const { executor, isRan, doExit } = pseudoIntervalParams;
+
+  if (isRan) {
+    executor().finally(() => {
+      if (!doExit) {
+        loop(pseudoIntervalParams);
+      }
+    });
+  } else if (!doExit) {
+    loop(pseudoIntervalParams);
+  }
 }
